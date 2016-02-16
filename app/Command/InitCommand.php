97eb007a31f895c53a6a2d799777a5f31c36316e
KaPJICoH <ruslan54@ukr.net>
2016-02-16 10:29:24 +0200

    Init command

--- app/Command/InitCommand.php
+++ app/Command/InitCommand.php
@@ -2,11 +2,16 @@
 
 namespace Mantainance\Command;
 
+use Mantainance\Driver\Apache as Apache;
+use Mantainance\Driver\Nginx as Nginx;
+use Mantainance\Driver\Check as Check; 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument as InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface as OutputInterface;
+
+
 
 class InitCommand extends Command {
 
@@ -20,7 +25,7 @@ class InitCommand extends Command {
                 'apache or nginx'
             )
             ->addArgument(
-                'config-path', 
+                'config-path',                 
                 InputArgument::REQUIRED, 
                 'Define path of server configuration file for your app'
             )
@@ -32,13 +37,48 @@ class InitCommand extends Command {
             ->setHelp('Help here');
 
         return;
-    }
-
-
+    }   
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('It Worked!'); 
+        
+        //shell_exec("sudo mkdir ~/mantainance");
+        
+        $name ;//for the future
+        $server=$input->getArgument('server-type');
+        $path_config=$input->getArgument('config-path');
+        $path_page=$input->getArgument('mantainance-page-path');
+       
+        
+        $server = strtolower($server);            
+        switch ($server) {
+
+            //nginx
+            case 'nginx':
+                $output->writeln( 'You choose nginx');
+                //$nginx =new Nginx\Driver();
+
+                if(Check::check_is_file($path_config) && Check::check_is_file($path_page)){
+                    $output->writeln('You work with nginx');            
+                    //$nginx->applySettings($path_config, $path_page);   
+                }  
+                break;
+
+            //apache    
+            case 'apache':
+                $output->writeln( "You choose apache");
+                $apache =new Apache\Driver();
+                if(Check::check_rewrite() && Check::check_is_file($path_config) && Check::check_is_file($path_page)){
+                    $output->writeln('You work with apache');
+                    $apache->applySettings($path_config, $path_page);   
+                }
+                break;
+
+
+            default:
+                $output->writeln("<error>You choose '$server' we dont work with it. Please chose Apache or Nginx</error>");               
+                break;
+        }      
 
         return;
-    }
+    }   
 }
--- app/Driver/Apache/Driver.php
+++ app/Driver/Apache/Driver.php
@@ -5,5 +5,52 @@ namespace Mantainance\Driver\Apache;
 use Mantainance\Driver\DriverInterface;
 
 class Driver implements DriverInterface {
-    
+    public function applySettings($pathToConfig, $pathToMantainancePage){
+        $file = file($pathToConfig);        
+        $new_file=[];        
+        $filename=basename($pathToMantainancePage);
+        $dirname =dirname($pathToMantainancePage);
+
+        //add dir and config file
+        if(!file_exists('/etc/apache2/conf')){
+        	shell_exec("sudo mkdir /etc/apache2/conf");
+        }
+        $home = getenv('HOME'); 
+        $conf=[ "Alias /errors ".$dirname."\n",
+        		"<If \"-f '".$home."/maintenance/maintenance.enable'\">\n",
+        		"	<IfModule mod_rewrite.c>\n",
+                "   	RewriteEngine On\n",                  		
+                "   	RewriteCond %{SCRIPT_FILENAME} !".$filename."\n",
+                "   	RewriteRule ^.*$ /".$filename." [R=503,L]\n",
+                "   	ErrorDocument 503 /errors/".$filename."\n",
+                "	</IfModule>\n",
+                "</If>\n"];
+
+        file_put_contents("maintenance.conf" , $conf);
+        shell_exec("sudo mv maintenance.conf /etc/apache2/conf/maintenance.conf -f");
+        
+        //add include
+        if(!preg_grep('/Include conf\/maintenance\.conf/', $file)){
+        	echo "dont have inlude\n";
+        	$key_virtual = array_keys(preg_grep( '/<virtualhost/i', $file))[0];
+        	foreach ($file as $key => $line) {   	
+        		array_push( $new_file, $line);
+        		if ($key == $key_virtual) {
+        			array_push($new_file, 	"	Include conf/maintenance.conf\n" );
+        		}
+        	}
+        	$config_name=basename("pathToConfig");
+        	file_put_contents($config_name, $new_file);
+        	shell_exec("sudo mv ".$config_name." ".$pathToConfig." -f");       	
+        }
+	
+        
+        
+        shell_exec("sudo service apache2 restart");
+    }
 }
+
+        
+          
+           
+
