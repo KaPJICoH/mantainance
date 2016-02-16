edcb72bbc87aa3edf91bc90e1e5476c0d02e2aea
KaPJICoH <ruslan54@ukr.net>
2016-02-16 17:20:21 +0200

    Add list command

---
+++ app/Command/ListCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mantainance\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument as InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface as OutputInterface;
+
+class ListCommand extends Command {
+
+    protected function configure()
+    {
+        $this->setName('list')
+            ->setDescription('List of names')
+            ->setHelp('Help here');
+            
+        return;
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {    
+        $home = getenv('HOME');    
+        $dir = $home."/maintenance/";
+        $list_name=[]; 
+        $files = array_diff(scandir($dir), array('..', '.'));
+        foreach ($files as $file) {
+            if(is_dir($dir.$file))
+                array_push($list_name, $file);
+        }
+        
+        $output->writeln('<info> List name:</info>');
+        foreach ($list_name as $key => $name) {
+            $key=$key+1;
+            $output->writeln("<info> ".$key.". ".$name."</info>");
+        }
+        
+        return;
+    }
+}
--- app/console.sh
+++ app/console.sh
@@ -7,8 +7,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Mantainance\Command\InitCommand;
 use Mantainance\Command\UpCommand;
 use Mantainance\Command\DownCommand;
+use Mantainance\Command\ListCommand;
 use Symfony\Component\Console\Application;
 
 $app = new Application('Mantainance', '0.0.1');  
-$app->addCommands([new InitCommand(), new UpCommand(), new DownCommand()]);
+$app->addCommands([new InitCommand(), new UpCommand(), new DownCommand(), new ListCommand()]);
 $app->run();
